26d61a83433629afa8798561b9e83ba09e505f9e
Philip Tecklenburg <Philip.Tecklenburg@lvs1.com>
2016-11-18 09:33:18 -0800

    fixing formatting

--- textcrawler.py
+++ textcrawler.py
@@ -2,6 +2,7 @@ import filefunctions
 import os
 import openpyxl
 
+#----Crawl directory for text files containing string, export results to Excel file-----
 directory = raw_input("directory to search: ")
 searchstring = raw_input("search string (not case sensitive): ")
 
@@ -13,3 +14,4 @@ filefunctions.searchdirforstring(directory,searchstring,reportsheet)
 
 report.save(filename = "results.xlsx")
 
+#----------------------------------------------------------------------------------------
